c512406c8202a14ae875fe6d666944e983fa13e7
Ayoub <ayoubzerou@gmail.com>
2024-07-28 04:15:35 -0700

    convert_c_percent_p.c

--- convert_c_percent_p.c
+++ convert_c_percent_p.c
@@ -78,6 +78,7 @@ unsigned int convert_percent(va_list args, buffer_t *output,
  *
  * Return: The number of bytes stored to the buffer.
  */
+
 unsigned int convert_p(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len)
 {
