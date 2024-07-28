b7464757db0f91644a6d4b4f41d4711f93da8907
Ayoub <ayoubzerou@gmail.com>
2024-07-28 04:21:28 -0700

    convert_numbers.c

--- convert_numbers.c
+++ convert_numbers.c
@@ -63,6 +63,7 @@ unsigned int convert_sbase(buffer_t *output, long int num, char *base,
  *
  * Return: The number of bytes stored to the buffer.
  */
+
 unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
 		unsigned char flags, int wid, int prec)
 {
